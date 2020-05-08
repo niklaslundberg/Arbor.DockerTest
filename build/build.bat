3a5707ba01cac5caeb0f528a5f98ccefda01d1c1
Niklas Lundberg <info@nlundberg.se>
2020-05-08 13:03:47 +0200

    Allow prerelease build tool.

--- build/build.bat
+++ build/build.bat
@@ -2,5 +2,6 @@
 
 SET Arbor.Build.Vcs.Branch.Name=%GITHUB_REF%
 SET Arbor.Build.BuilderNumber.UnixEpochSecondsEnabled=true
+SET Arbor.Build.Bootstrapper.AllowPrerelease=true
 
 call dotnet arbor-build
