629c2bed90bca5fb99cab5eda5eb7dedbb2d07b6
Niklas Lundberg <info@nlundberg.se>
2022-11-10 21:00:48 +0100

    Redis

--- src/Arbor.Docker.Xunit.Tests.Integration/SampleDockerTest.cs
+++ src/Arbor.Docker.Xunit.Tests.Integration/SampleDockerTest.cs
@@ -18,7 +18,7 @@ namespace Arbor.Docker.Xunit.Tests.Integration
 
         protected override async IAsyncEnumerable<ContainerArgs> AddContainersAsync()
         {
-            var portMappings = new[] {new PortMapping(new PortRange(3125), new PortRange(80)), MapSinglePort(2526, 25)};
+            var portMappings = new[] {new PortMapping(new PortRange(3125), new PortRange(80)), MapSinglePort(12526, 25)};
             yield return new ContainerArgs(
                 "rnwood/smtp4dev:linux-amd64-v3",
                 "smtp4devtest",
@@ -27,7 +27,7 @@ namespace Arbor.Docker.Xunit.Tests.Integration
             );
         }
 
-        [Fact(Skip = "Environment dependent")]
+        [Fact]
         public async Task SendMail()
         {
             var message = new MimeMessage();
@@ -44,7 +44,7 @@ namespace Arbor.Docker.Xunit.Tests.Integration
 
                 client.AuthenticationMechanisms.Remove("XOAUTH2");
 
-                await client.ConnectAsync("localhost", 2526, false).ConfigureAwait(false);
+                await client.ConnectAsync("localhost", 12526, false).ConfigureAwait(false);
 
                 await client.SendAsync(message).ConfigureAwait(false);
                 await client.DisconnectAsync(true).ConfigureAwait(false);
